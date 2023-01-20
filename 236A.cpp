60fc9186cd2d1d7bdba7f6b293397eb36eb69268
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-01-20 12:04:31 +1100

    236A

---
+++ 236A.cpp
@@ -0,0 +1,30 @@
+// Boy or Girl: https://codeforces.com/problemset/problem/236/A
+
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("236A.in", "r", stdin);
+    #endif
+
+    unordered_set<char> us;
+
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++) {
+        us.insert(s.at(i));
+    }
+
+    if (us.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    } else {
+        cout << "IGNORE HIM!" << endl;
+    }
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-BINS=4A 71A
+BINS=4A 71A 236A
 
 $(BINS):
-	g++ --std=c++20 -Wall -o $@ $@.cpp
+	g++ --std=c++20 -o $@ $@.cpp
 	./$@
 	rm -rf $(BINS)
 
