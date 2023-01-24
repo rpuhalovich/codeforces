550a11bbcc07b84d011c09db8c866bb129917fc7
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-01-24 22:21:09 +1100

    Updated makefile flags.

--- 1772C.cpp
+++ 1772C.cpp
@@ -1,30 +1,23 @@
 // https://codeforces.com/problemset/problem/1772/C
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_set>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int k, n;
 
-int characteristic(const vector<int>& arr) {
-    unordered_set<int> st;
-    vector<int> resarr;
-
-    for (int i = 1; i < arr.size(); i++) {
-        int val = arr[i] - arr[i - 1];
-        resarr.push_back(val);
-        st.insert(val);
+void setup(int k, int n) {
+    int cache[41];
+    for (int i = 0; i < k; i++) {
+        cache[i] = (i + 2) - (i + 1);
     }
-
-    return st.size();
+    for (int i : cache) cout << i << " ";
+    cout << endl;
 }
 
 void solve() {
     cin >> k >> n;
-    cout << k << " " << n << endl;
+    setup(k, n);
 }
 
 int main() {
---
+++ contest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("test.in", "r", stdin);
+#endif
+    return 0;
+}
