390e7810ed954bbba78647a9d54a63a2660ffd8a
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-02-07 16:57:48 +1100

    276B

--- 276B.cpp
+++ 276B.cpp
@@ -1,7 +1,12 @@
-//
+// https://codeforces.com/problemset/problem/276/B
+
+// Remember to read the question properly - you can rearrange the characters
+// therefore dp wasn't necessary.
 
 #include <bits/stdc++.h>
+
 using namespace std;
+
 #define ll long long
 #define nl '\n'
 #define map unordered_map
@@ -16,9 +21,25 @@ template<typename T>
 void logArr(T& a,int n){cout<<"[ ";for(int i=0;i<n;i++)cout<<a[i]<<" ";cout<<"]"<<nl;}
 template<typename T1,typename T2>
 void setArr(T1& a,int n,T2 v){for(int i=0;i<n;i++)a[i]=v;}
-bool isPali(string s,int l,int r){while(l<r)if(s[l++]!=s[r--])return false;return true;}
 
+int counts[26];
+string s;
 void solve() {
+    cin >> s;
+    if(s.size()==1){cout<<"First"<<nl;return;}
+    setArr(counts, 26, 0);
+    for(auto c:s) {
+        counts[c-'a'] += 1;
+    }
+    int isOdd = 0;
+    for(int i:counts) {
+        if(i&2!=0) isOdd++;
+    }
+    if(isOdd==1||isOdd==0){
+        cout << "First" << nl;
+        return;
+    }
+    cout << ((isOdd-1)%2==0?"First":"Second")<<nl;
 }
 
 int main() {
--- 298B.cpp
+++ 298B.cpp
@@ -1,4 +1,4 @@
-//
+// https://codeforces.com/problemset/problem/298/B
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -18,7 +18,25 @@ template<typename T1,typename T2>
 void setArr(T1& a,int n,T2 v){for(int i=0;i<n;i++)a[i]=v;}
 bool isPali(string s,int l,int r){while(l<r)if(s[l++]!=s[r--])return false;return true;}
 
+int t, sx, sy, ex, ey;
+string dirs;
 void solve() {
+    cin>>t>>sx>>sy>>ex>>ey;
+    cin>>dirs;
+
+    int time=0;
+    for(int i=0;i<t;i++) {
+        if(dirs[i]=='N'&&abs(ey-sy)>abs(ey-sy+1))sy++;
+        if(dirs[i]=='S'&&abs(ey-sy)>abs(ey-sy-1))sy--;
+        if(dirs[i]=='E'&&abs(ex-sx)>abs(ex-sx+1))sx++;
+        if(dirs[i]=='W'&&abs(ex-sx)>abs(ex-sx-1))sx--;
+        time++;
+        if(sx==ex&&sy==ey) {
+            cout<<time<<nl;
+            return;
+        }
+    }
+    cout<<-1<<nl;
 }
 
 int main() {
