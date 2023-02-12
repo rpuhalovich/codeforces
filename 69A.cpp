2dafccf08140cc6d6c0afcab0969d1e001091a69
Ryan Puhalovich <reanpuhalovich@gmail.com>
2023-02-12 15:23:50 +1100

    69A nice

---
+++ 69A.cpp
@@ -0,0 +1,43 @@
+// https://codeforces.com/problemset/problem/69/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define f0r(a, b) for (long long a = 0; a < (b); ++a)
+#define f1r(a, b, c) for (long long a = (b); a < (c); ++a)
+#define f0rd(a, b) for (long long a = (b); a >= 0; --a)
+#define f1rd(a, b, c) for (long long a = (b); a >= (c); --a)
+#define nl '\n'
+#define ll long long
+#define umap unordered_map
+#define uset unordered_set
+#define f first
+#define s second
+const int inf = INT_MAX;
+const int ninf = INT_MIN;
+
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v){cout<<"[ ";for(int i=0;i<v.size();i++){cout<<v[i]<<" ";}return cout<<"]";}
+template<typename T> void sort(vector<T>& v){sort(v.begin(),v.end());}
+template<typename T> void rsort(vector<T>& v){sort(v.rbegin(),v.rend());}
+template<typename T1,typename T2> void seta(T1& a,int n,T2 v){for(int i=0;i<n;i++)a[i]=v;}
+
+void solve() {
+    int n;cin >> n;
+    int x = 0, xin, y = 0, yin, z = 0, zin;
+    while (n--) {
+        cin >> xin >> yin >> zin;
+        x += xin;
+        y += yin;
+        z += zin;
+    }
+    cout << (x == 0 && y == 0 && z == 0 ? "YES" : "NO") << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
